7d8320b09727fe066227d6bdc24b5c72906bba6f
ShawnBlair <shawnmwa98@gmail.com>
2023-08-20 13:13:09 +0300

    m

--- src/App.js
+++ src/App.js
@@ -18,17 +18,34 @@ function App() {
         </a> */}
 
 //https://reactchallenges.live/challenge/2 - CHECK FOR CHALLENGES HERE {SETINTERVAL() FOR TIME}
-        const [state, setState] = React.useState(true)
+        const [state, setState] = React.useState(0)
         
-        function toggle(){
-setState(!state)
+        function start(){
+          //we're supposed to use useEffect for this but since we're not setInterval will return something
+          //related to the window of the browser ie window.{...} instead of the usual constant i.e const {...}
+           window.treturn = setInterval(() => {
+            setState(state => state + 1)
+                      }, 1000)
         }
+
+        function stop(){
+          clearInterval(window.treturn)
+                  }
+
+                  function reset(){
+                    clearInterval(window.treturn)
+                    setState(0)}
 
   return (
     <div className="App">
       <header className="App-header">
-        <button onClick={toggle}>Toggle</button>
-        {state && <h3>Show information</h3>}
+        <h2>Timer</h2>
+        <span>{Math.trunc(state / 60)} hrs : </span>
+        <span>{Math.trunc(state / 60)} mins : </span>
+      <span>{state % 60} secs</span>
+        <button onClick={start}>Start</button>
+        <button onClick={reset}>Reset</button>
+        <button onClick={stop}>Stop</button>
       </header>
     </div>
   );
