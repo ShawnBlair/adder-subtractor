923115b6909d09783d7d6d9cb30e8d9e490788c2
ShawnBlair <shawnmwa98@gmail.com>
2023-09-01 12:36:51 +0300

    m

--- src/App.js
+++ src/App.js
@@ -203,22 +203,23 @@ const [factor, setFactor] = React.useState(0)
 
 function handleChange(event){
   setNumber(event.target.value)
+  setFactor( factor =>
+    {if(number === 0){
+      return 1;
+    }else{
+      return number * (number - 1)
+    }}
+  )
 }
 
 function factored(){
-setFactor( factor =>
-  {if(number === 0){
-    return 1;
-  }else{
-    return number * (number - 1)
-  }}
-)
+
 }
 
   return (
     <div className="App">
       <header className="App-header">        
-        <div className="number-div">The factorial of <input type='text' value={number} onChange={handleChange}></input> is {factored}</div>
+        <div className="number-div">The factorial of <input type='text' value={number} onChange={handleChange}></input> is {factor}</div>
          <button>Re-Render</button>     
       </header>
     </div>
