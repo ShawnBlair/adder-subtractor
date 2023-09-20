c2990b423a84145fe117162fddf787986bacafd2
ShawnBlair <shawnmwa98@gmail.com>
2023-09-20 09:20:55 +0300

    m

--- src/App.js
+++ src/App.js
@@ -304,7 +304,7 @@ const [nset, setNset] = React.useState(
   const listItems = document.querySelectorAll('li a');
   listItems.forEach((item) => {
     item.addEventListener('click', (event) => {const href = event.target.getAttribute('href');
-    setNset({ href });});
+    setNset( href );});
   });
 //}<p>Clicked Href: {nset.href}</p>
 
