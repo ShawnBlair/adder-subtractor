215c011d7b106856425b0506abfbefc888bcc727
ShawnBlair <109588759+ShawnBlair@users.noreply.github.com>
2023-11-11 21:25:54 +0300

    Delete src/Ram.js

--- src/Ram.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-export default function Ram(props){
-    //TO CLASSIFY WHETHER IT'S AN EMPLOYEE OR AN ID CHECK YOUR CHATGPT'S 'DISPLAY LINK HREF ONCLICK...'
-    return(
-        <div className="Ram" ><p >Employee: {props.nset}</p></div>
-    )
-}
